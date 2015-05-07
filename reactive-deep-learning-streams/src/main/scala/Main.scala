cf90b8a9e7f3caf650c67b181596f0ea849bf640
martinzapletal <zapletal-martin@email.cz>
2015-05-07 14:24:02 +0100

    Fixed input layer input vectors

--- reactive-deep-learning-streams/src/main/scala/Main.scala
+++ reactive-deep-learning-streams/src/main/scala/Main.scala
@@ -38,25 +38,25 @@ object Main extends App {
       val zip2 = builder.add(Zip[Seq[Double], Seq[Double]]())
 
       val perceptron = Flow[(Seq[Double], Seq[Double])]
-        .map(i => Neuron.sigmoid(i._1.zip(i._2).map(x => x._1 * x._2).sum)) //+ bias)
+        .map(i => Neuron.sigmoid(i._1.zip(i._2).map(x => x._1 * x._2).sum + 0.2))
 
-      val merge = builder.add(Merge[Double](2))
-
+      val outputLayerMerge = builder.add(Merge[Double](2))
       val zipWithIndex = builder.add(Zip[Double, Int]())
-
       val index = Source(() => Iterator.from(0, 1))
       val out = Sink.foreach(println)
+      val inputVectorSplit = builder.add(Broadcast[Seq[Double]](2))
 
-      input ~> zip.in0
-      input ~> zip2.in0
-      hiddenLayer ~> weightSplit ~> unzip.in
-                                    unzip.out0 ~> zip.in1
-                                    unzip.out1 ~> zip2.in1
-                                                  zip.out ~> perceptron ~> merge.in(0)
-                                                  zip2.out ~> perceptron ~> merge.in(1)
-                                                                           merge.out ~> zipWithIndex.in0
-                                                                           index ~> zipWithIndex.in1
-                                                                                    zipWithIndex.out ~> out
+      input ~> inputVectorSplit.in
+               inputVectorSplit.out(0) ~> zip.in0
+               inputVectorSplit.out(1) ~> zip2.in0
+               hiddenLayer ~> weightSplit ~> unzip.in
+                                             unzip.out0 ~> zip.in1
+                                             unzip.out1 ~> zip2.in1
+                                                           zip.out ~> perceptron ~> outputLayerMerge.in(0)
+                                                           zip2.out ~> perceptron ~> outputLayerMerge.in(1)
+                                                                                     outputLayerMerge.out ~> zipWithIndex.in0
+                                                                                     index ~> zipWithIndex.in1
+                                                                                              zipWithIndex.out ~> out
     }
 
     .run()
