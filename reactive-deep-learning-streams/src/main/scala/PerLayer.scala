f2cd4e5f63a492cf5a582694cdea805d1497785e
martinzapletal <zapletal-martin@email.cz>
2015-05-08 21:15:54 +0100

    Reactive stream implementation of per-layer neural network using (sub flow)^* composition

--- reactive-deep-learning-streams/src/main/scala/PerLayer.scala
+++ reactive-deep-learning-streams/src/main/scala/PerLayer.scala
@@ -1,7 +1,7 @@
 import java.text.SimpleDateFormat
 import java.util.Date
 
-import akka.stream.FanInShape2
+import akka.stream.{FlowShape, SinkShape, Outlet, FanInShape2}
 import akka.stream.scaladsl._
 import breeze.linalg.{*, DenseMatrix}
 import breeze.numerics.sigmoid
@@ -9,7 +9,6 @@ import breeze.numerics.sigmoid
 object PerLayer {
 
   def graph(input: Source[DenseMatrix[Double], Unit]) = {
-    val format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS")
 
     val bias = 0.2
     val topology = Array(3, 2, 1)
@@ -41,24 +40,53 @@ object PerLayer {
       }
     }
 
+    def network(topology: Array[Int]) = {
+
+      FlowGraph.partial() { implicit builder: FlowGraph.Builder[Unit] =>
+        import FlowGraph.Implicits._
+
+        def buildLayer(layer: Int, input: Outlet[DenseMatrix[Double]], topology: Array[Int]): Outlet[DenseMatrix[Double]] = {
+          val layer1 = builder.add(hiddenLayer(layer))
+
+          input                     ~> layer1.in0
+          hiddenLayerWeights(layer) ~> layer1.in1
+
+          if (layer < topology.length - 1) buildLayer(layer + 1, layer1.out, topology) else layer1.out
+        }
+
+        val flow = builder.add(Flow[DenseMatrix[Double]])
+        val network = buildLayer(1, flow.outlet, topology)
+
+        new FlowShape(flow.inlet, network)
+      }
+    }
+
+    val zipWithIndex =
+      FlowGraph.partial() { implicit builder: FlowGraph.Builder[Unit] =>
+        import FlowGraph.Implicits._
+
+        val zipWithIndex = builder.add(Zip[DenseMatrix[Double], Int]())
+        val index = Source(() => Iterator.from(0, 1))
+
+        index ~> zipWithIndex.in1
+                 zipWithIndex.out
+
+        new FlowShape(zipWithIndex.in0, zipWithIndex.out)
+      }
+
+    val format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS")
+
+    val formatPrintSink =
+      Sink
+        .foreach((x: (DenseMatrix[Double], Int)) =>
+        println(s"Output ${x._2} with result ${x._1}in${format.format(new Date(System.currentTimeMillis()))}"))
+
     FlowGraph.closed() { implicit builder: FlowGraph.Builder[Unit] =>
       import FlowGraph.Implicits._
 
-      val zipWithIndex = builder.add(Zip[DenseMatrix[Double], Int]())
-      val index = Source(() => Iterator.from(0, 1))
-      val out = Sink
-        .foreach((x: (DenseMatrix[Double], Int)) => println(s"Output ${x._2} with result ${x._1}in${format.format(new Date(System.currentTimeMillis()))}"))
+      val inputOutlet = builder.add(Flow[DenseMatrix[Double]])
 
-      val layer1 = builder.add(hiddenLayer(1))
-      val layer2 = builder.add(hiddenLayer(2))
-
-      input                 ~> layer1.in0
-      hiddenLayerWeights(1) ~> layer1.in1
-                               layer1.out            ~> layer2.in0
-                               hiddenLayerWeights(2) ~> layer2.in1
-                                                        layer2.out ~> zipWithIndex.in0
-                                                             index ~> zipWithIndex.in1
-                                                                      zipWithIndex.out ~> out
+      input ~> inputOutlet ~> network(topology) ~> zipWithIndex ~> formatPrintSink
     }
   }
 }
